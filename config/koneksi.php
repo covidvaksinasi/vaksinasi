0d54315f93204786645fbf594dd490997ba82619
covidvaksinasi <Covidvaksinasi87@gmail.com>
2022-08-12 18:05:30 +0700

    koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,10 +2,10 @@
 //KONVERSI PHP KE PHP 7
 require_once "parser-php-version.php";
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "vaksinasi";
+$server = "remotemysql.com:3306";
+$username = "p23rQbGkpf";
+$password = "Rr0QG92Cgn";
+$database = "p23rQbGkpf ";
 
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
 mysql_select_db($database) or die("Maaf, Database tidak bisa dibuka");
